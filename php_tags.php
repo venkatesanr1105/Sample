53a60f6fcc4bc812cdb108de02998a3e7954c662
Venkatesan Rajendran <83106898+venkatesanr1105@users.noreply.github.com>
2022-09-02 15:17:02 +0530

    Add files via upload

---
+++ php_tags.php
@@ -0,0 +1,9 @@
+<?php echo 'if you want to serve PHP code in XHTML or XML documents, use these tags<hr />';?>
+
+<?= 'print this string.<hr />' ?>
+
+<? echo 'this code is within short tags, but will only work '.
+	'if short_open_tag is enabled';?>
+	
+<?php
+	echo 'Hello Aspire';
